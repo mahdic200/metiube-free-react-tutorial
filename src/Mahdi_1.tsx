989187601bbd5ec03d00e442ca70c132c4ccef34
mahdic200 <rmahdi170@gmail.com>
2023-09-29 17:21:36 +0330

    episode03 modules and react components

---
+++ src/Mahdi_1.tsx
@@ -0,0 +1,5 @@
+function Mahdi_1() {
+    return <p>Mahdi_1</p>;
+}
+
+export default Mahdi_1;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Mahdi_1 from "./Mahdi_1"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Mahdi_1 />
   </React.StrictMode>,
 )
