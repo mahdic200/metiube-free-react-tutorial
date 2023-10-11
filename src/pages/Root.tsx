dc82cfdbf83e8a51d3a6ce48abb94bf905cb80d4
mahdic200 <rmahdi170@gmail.com>
2023-10-11 16:28:45 +0330

    episode07 , type annotation in typescript

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from "react";
+
+interface mahdi {
+    style: string;
+    text: string;
+    position?: 1 | 2 | 3 | 4;
+    sayHello: (...args: any) => string;
+}
+
 
 function Root() {
-    const [count, setCount] = useState(0);
-    const [reRender, setReRender] = useState(true);
-    
-    useEffect(() => {
-        console.log('useEffect function executed !');
-    }, []);
+    // var myVar: number | string | boolean | Object | undefined | null | any;
+    // const handleChange: () => (void | string | null | undefined) 
+    // const handleChange: (arg1?: string | null, arg2?: boolean, arg3?: number | undefined) => void 
+    // const handleChange: (...args: any) => any = () => {};
+    // const handleChange: (arg1: Object) => void = () => {};
+    const handleChange: (arg1: mahdi) => void = () => {};
+    handleChange({
+        style: "mahdi",
+        text: "some text",
+        position: 1,
+        sayHello: () => {return ""}
+    });
 
     return (
         <section>
-            <section onClick={() => {
-                setCount(count + 1);
-            }}>root page count is : {count}
-            </section>
-            <button onClick={() => setReRender(!reRender)}>change</button>
+            
         </section>
     );
 }
